d0d1ea6dbbb39391775820fd7257a91f956446bc
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-15 14:24:43 -0400

    Defined seter and getters for song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,25 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    # artist ? artist.name : nil
+    artist&.name
+  end
+
+  def note_contents=(contents)
+    self.notes = contents.map do |content|
+      Note.find_or_create_by(content: content)
+    end
+  end
+
+  def note_contents
+    self.notes.map { |note| note.content}
+  end
+
 end
